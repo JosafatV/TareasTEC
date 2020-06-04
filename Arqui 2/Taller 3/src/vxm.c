961f66761cd9e0a509d6fb755c92a3fed61c1a5c
JosafatV <josav09@gmail.com>
2020-06-04 11:35:48 -0600

    Fixed vxm

--- Arqui 2/Taller 3/src/vxm.c
+++ Arqui 2/Taller 3/src/vxm.c
@@ -8,38 +8,53 @@ int main () {
 
 	__m128i vector = _mm_set_epi32 (11, 29, 69, 72);
 	
-	__m128i m0 = _mm_set_epi32 (1, 1, 1, 1);
-	__m128i m1 = _mm_set_epi32 (1, 2, 1, 1);
-	__m128i m2 = _mm_set_epi32 (1, 1, 5, 1);
-	__m128i m3 = _mm_set_epi32 (1, 1, 1, 2);
-	
-	__m128i r0 = _mm_mul_epi32(m0, vector);
-	__m128i r1 = _mm_mul_epi32(m1, vector);
-	__m128i r2 = _mm_mul_epi32(m2, vector);
-	__m128i r3 = _mm_mul_epi32(m3, vector);
+	// Row 0				Col:	0	  1     2     3
+	__m128i m0a = _mm_set_epi32 (0, 1, 0, 1);
+				__m128i m0b = _mm_set_epi32 (0, 2, 0, 1);
+	// Row 1						|     |     |     |
+	__m128i m1a = _mm_set_epi32 (0, 1, 0, 1);
+				__m128i m1b = _mm_set_epi32 (0, 1, 0, 2);
+	// Row 2						|     |     |     |
+	__m128i m2a = _mm_set_epi32 (0, 1, 0, 1);
+				__m128i m2b = _mm_set_epi32 (0, 2, 0, 1);
+	// Row 3						|     |     |     |
+	__m128i m3a = _mm_set_epi32 (0, 1, 0, 1);
+				__m128i m3b = _mm_set_epi32 (0, 1, 0, 2);
+
+	// _mm_mul multpiplies the lower 32 bits of m and stores them in 64 bits of r
+	__m128i r0a, r0b, r1a, r1b, r2a, r2b, r3a, r3b;
+	r0a = _mm_mul_epi32(m0a, vector);
+	r0b = _mm_mul_epi32(m0b, vector);
+	r1a = _mm_mul_epi32(m1a, vector);
+	r1b = _mm_mul_epi32(m1b, vector);
+
+	r2a = _mm_mul_epi32(m2a, vector);
+	r2b = _mm_mul_epi32(m2b, vector);
+	r3a = _mm_mul_epi32(m3a, vector);
+	r3b = _mm_mul_epi32(m3b, vector);
 
 	int data = 0;
 
 	printf("+++ Matriz resultante: +++\n \t");
-	data = _mm_extract_epi32(r0, 3); 	printf("%d \t", data);
-	data = _mm_extract_epi32(r0, 2); 	printf("%d \t", data);
-	data = _mm_extract_epi32(r0, 1); 	printf("%d \t", data);
-	data = _mm_extract_epi32(r0, 0); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r0a, 2); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r0a, 0); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r0b, 2); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r0b, 0); 	printf("%d \t", data);
 	printf("\n \t");
-	data = _mm_extract_epi32(r1, 3); 	printf("%d \t", data);
-	data = _mm_extract_epi32(r1, 2); 	printf("%d \t", data);
-	data = _mm_extract_epi32(r1, 1); 	printf("%d \t", data);
-	data = _mm_extract_epi32(r1, 0); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r1a, 2); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r1a, 0); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r1b, 2); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r1b, 0); 	printf("%d \t", data);
 	printf("\n \t");
-	data = _mm_extract_epi32(r2, 3); 	printf("%d \t", data);
-	data = _mm_extract_epi32(r2, 2); 	printf("%d \t", data);
-	data = _mm_extract_epi32(r2, 1); 	printf("%d \t", data);
-	data = _mm_extract_epi32(r2, 0); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r2a, 2); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r2a, 0); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r2b, 2); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r2b, 0); 	printf("%d \t", data);
 	printf("\n \t");
-	data = _mm_extract_epi32(r3, 3); 	printf("%d \t", data);
-	data = _mm_extract_epi32(r3, 2); 	printf("%d \t", data);
-	data = _mm_extract_epi32(r3, 1); 	printf("%d \t", data);
-	data = _mm_extract_epi32(r3, 0); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r3a, 2); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r3a, 0); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r3b, 2); 	printf("%d \t", data);
+	data = _mm_extract_epi32(r3b, 0); 	printf("%d \t", data);
 	printf("\n");
 
 	return 0;
