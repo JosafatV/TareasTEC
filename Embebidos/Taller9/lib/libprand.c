7ab6787b3b2c9487072ce2a0b2c39e7c393a9afe
JosafatV <josav09@gmail.com>
2020-06-25 16:04:35 -0600

    compiling w cmake

--- Embebidos/Taller9/lib/libprand.c
+++ Embebidos/Taller9/lib/libprand.c
@@ -2,28 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-//#include <omp.h>
 
-int chrono_flag = 0;
-double start_time = 0;
 char* PATH = "/dev/timer";
-
-/** Toggle an execution timer. Call to start timer or to stop and print exe time
-
-void chrono () {
-	double current_time = omp_get_wtime();
-	// Running, set start_time
-	if (chrono_flag) {
-		chrono_flag = 0;
-		printf("Execution time is: %.6f\n", current_time-start_time);
-		start_time=0;
-	} else {
-		chrono_flag = 1;
-		start_time = current_time;
-		printf("Timer started\n");
-	}
-}
-*/
 
 /** Writes a int to a file, overwritting the existing file
  * \param data the data to be written. An int
