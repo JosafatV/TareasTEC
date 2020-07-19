f29ae3d1c9d59ed16b17b239308ec1eef7db9d40
JosafatV <josav09@gmail.com>
2020-07-19 11:07:27 -0600

    A2 T6 files and updated readme

--- Embebidos/.memory.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/josav09/Documents/Git/TareasTEC/Embebidos/memory.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/josav09/Documents/Git/TareasTEC/Embebidos/memory.ko /home/josav09/Documents/Git/TareasTEC/Embebidos/memory.o /home/josav09/Documents/Git/TareasTEC/Embebidos/memory.mod.o ;  true
--- Embebidos/.memory.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/josav09/Documents/Git/TareasTEC/Embebidos/memory.mod := { echo  /home/josav09/Documents/Git/TareasTEC/Embebidos/memory.o; echo; } > /home/josav09/Documents/Git/TareasTEC/Embebidos/memory.mod
