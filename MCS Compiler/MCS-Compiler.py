2eed285052d79cad0b767c40a8459916f69f735d
JosafatV <josav09@gmail.com>
2020-11-04 22:20:28 -0600

    Added DS3 manual update proposition, updated compiler and added template files

--- MCS Compiler/MCS-Compiler.py
+++ MCS Compiler/MCS-Compiler.py
@@ -23,7 +23,7 @@ def parse_input ():
     #job_lists = [["ffst", 0, 1000], ["ssnd",1000,2000], ["ttrd",2000,1000],["ffth", 1000, 500], ["vvth", 1000, 500], ["ssth", 1000, 0]] 
 
     # identify relationships [task_id_src, task_id_dest, data_volume]
-    job_relations = [[0,1,100], [1,2,100], [2,3,100]]
+    job_relations = [[0,1,100], [1,2,100]]
     #job_relations = [[0,1,100], [1,2,100], [2,3,100], [2,4,100], [4,5,100], [3,5,100]]
 
     # create file
@@ -33,7 +33,7 @@ def parse_input ():
     master_string += def_tasks + str(len(job_lists)) + newline
 
     print ("Creating File")
-    output_file_name = "converted_"+input_file_name
+    output_file_name = "job_"+input_file_name
     writeFile(output_file_name, master_string)
 
     # add tasks to file
@@ -51,7 +51,7 @@ def parse_input ():
         if (task_id == len(job_lists)-1):
             master_string += job_lists[task_id][0] + " " + tail + newline
         #set values for task parameters
-        master_string += job_lists[task_id][0] + " earliest start 0 deadline 10000 " + "input_vol " + str(job_lists[task_id][1]) + " output_vol " +  str(job_lists[task_id][2]) + newline
+        master_string += job_lists[task_id][0] + " earliest_start 0 deadline 10000 " + "input_vol " + str(job_lists[task_id][1]) + " output_vol " +  str(job_lists[task_id][2]) + newline
         appendFile(output_file_name, master_string)
 
     # add communication volumes to file
