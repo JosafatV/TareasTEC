1724440bdd6f093ded4e806223a326533796e63b
JosafatV <josav09@gmail.com>
2022-02-10 16:25:55 -0600

    Updated files
    and new version of the FileManager

--- Python Tests/decorators.py
+++ Python Tests/decorators.py
@@ -1,66 +1,99 @@
-# Decorators are usually used with classes!
-
 import random
 
-# Roll a dice and automaticall reroll it if it is below reroll
-# returns minimum r if r is specified
 
 class DiceRoller:
+    """
+    Roll a dice and automaticall reroll it if it is below reroll
+    WIP: returns minimum r if r is specified
+    """
 
-	@property    
-	def d20(self):
-		return random.randint(1, 20)
-	@property    
-	def d12(self):
-		return random.randint(1, 12)
-	@property    
-	def d10(self):
-		return random.randint(1, 10)
-	@property    
-	def d8(self):
-		return random.randint(1, 8)
-	@property    
-	def d6(self):
-		return random.randint(1, 6)
-	@property    
-	def d4(self):
-		return random.randint(1, 4)
+    @property
+    def d20(self):
+        return random.randint(1, 20)
 
+    @property
+    def d12(self):
+        return random.randint(1, 12)
 
-# By defining the the functions as property we can call them
-# as attributes of the class instead of methods (roll.d20())
-roll = DiceRoller()
-print(f' Rolling a d20: {roll.d20}')
-print(f' Rolling a d12: {roll.d12}')
-print(f' Rolling a d10: {roll.d10}')
-print(f' Rolling a d8: {roll.d8}')
-print(f' Rolling a d6: {roll.d6}')
-print(f' Rolling a d4: {roll.d4}')
+    @property
+    def d10(self):
+        return random.randint(1, 10)
+
+    @property
+    def d8(self):
+        return random.randint(1, 8)
+
+    @property
+    def d6(self):
+        return random.randint(1, 6)
+
+    @property
+    def d4(self):
+        return random.randint(1, 4)
 
 
+if __name__ == "__main__":
+    # By defining the the functions as property we can call them
+    # as attributes of the class instead of methods (roll.d20())
+    roll = DiceRoller()
+    print(f' Rolling a d20: {roll.d20}')
+    print(f' Rolling a d12: {roll.d12}')
+    print(f' Rolling a d10: {roll.d10}')
+    print(f' Rolling a d8: {roll.d8}')
+    print(f' Rolling a d6: {roll.d6}')
+    print(f' Rolling a d4: {roll.d4}')
 
 
+def my_hof(func):
+    def wrapper():
+        print("+++ ", end="")
+        func()
+        print("+++ ")
+    return wrapper()
 
 
+# This decorator allows us to run an "amplified" version of hello() by running my_hof(hello)
+# Note: not sure why is making the call automatically
+@my_hof
+def my_hello():
+    print("Hello world!", end=" ")
 
-# General implementation of the Wounding Table
-# returns the minimal required value to wound
-def wound (S, T):
-	if (S == T):
-		print ("4+")
-		return 4
-	elif (S<=T):
-		if (S*2 <= T):
-			print ("6+")
-			return 6
-		else:
-			print ("5+")
-			return 5
-	else:
-		if (S >= T*2):
-			print ("2+")
-			return 2
-		else:
-			print ("3+")
-			return 3
-	
+
+def my_hof2(func):
+    def wrapper(param="cruel world"):
+        print("+++ End transmission: ", end="")
+        func(param)
+        print("+++ ")
+    return wrapper()
+
+# This decorator has parameters
+
+
+@my_hof2
+def my_bye(name):
+    print(f"Good bye, {name}", end=" ")
+
+# Note: not sure why is also not accepting the parameter
+# my_bye("Aritosteles")
+# my_hof2(my_bye("Aristoteles"))
+
+
+def my_hof3(func):
+    def wrapper(*args, **kwargs):
+        print(" >> ", end="")
+        func(*args, **kwargs)
+        print("")
+    return wrapper()
+
+# This decorator fixes issues with multiple parameter. No longer needing to change parameters in wrapper, func call and func definition
+# Again no idea why is it instant-calling and this one I can't patch to run
+
+
+"""
+@my_hof3
+def transmit(words, date, time, to):
+    print(f"{date}, {time} to {to}: {words} :: ")
+
+
+transmit("Requisiton denied", "M41.132", "05:30", "ME-4ErOs24")
+"""
--- Python Tests/fileManager.py
+++ Python Tests/fileManager.py
@@ -1,123 +1,81 @@
-# Instituto Tecnologico de Costa Rica
+# Kyndryl
 #
 # Python file manager
 #
 # from fileManager import *
 #
 # Josafat Vargas
-# 30 of May, 2019
+# 4th of Feb, 2022
 
-
-# Manages the access and operaions in the external file
-# fileName: file to be accessed
-# mode: "r" to read, "w" to write (erases previous data), "a" appends data to file
-# data: anything if read, data to append/wrie
-# return: 0 if failed
-# return: 1 if succeded, overwrites/appends to fileName file
-# return: data read if mode == "r"
-def fileManager (fileName, mode, data):
-    #read and print file
-    if (mode == "r"):
-        file_object = open(fileName, mode)
-        return file_object.read()
-    #appends to file
-    if (mode == "a"):
-        file_object = open(fileName, mode)
-        file_object.write(data)
+def readFile(fileName):
+    """ Opens, reads and then closes a file. Returns the information in the file as a string
+    fileName (str): file to be accessed ie: "myfile.txt", "myFolder/myFile.txt".
+    return: string with the information in the file
+    return: int if error
+    """
+    try:
+        file_object = open(fileName, "r")
+        read_data = file_object.read()
         file_object.close()
-        return 0
-    #Create a new file
-    if (mode == "w"):
-        file_object = open(fileName, mode)
-        file_object.write(data)
-        file_object.close()
-        return 0
-    else:
+        return read_data
+    except:
+        print("+++ Read operation failed +++")
         return 1
 
 
-
-# Opens a txt file and captures it's contents
-#
-# str fileName: file to be accessed ie: "myfile.txt", "myFolder/myFile.txt"
-# 
-# return: string with data
-def readFile (fileName):
-    file_object = open(fileName, "r")
-    read_data =  file_object.read()
-    file_object.close()
-    return read_data
-
-
-
-# Creates a new text file and fills it with data. It overwrites exisiting files
-#
-# str fileName: file to be accessed ie: "myfile.txt", "myFolder/myFile.txt".
-#               creates a file if it doesn't exist
-# str toWrite: data to write the file
-# 
-# return: 0 when done
-def writeFile (fileName, toWrite):
-    file_object = open(fileName, "w")
-    file_object.write(toWrite)
-    file_object.close()
-    return 0
-
-
-# Appends data to an already existing file
-#
-# str fileName: file to be accessed ie: "myfile.txt", "myFolder/myFile.txt".
-# str toWrite: data to write the file
-# 
-# return: 0 when done
-def appendFile (fileName, toWrite):
-    file_object = open(fileName, "a")
-    file_object.write(toWrite)
-    file_object.close()
-    return 0
-
-
-
-# Test
-# Create a file, and write to it. Append to file. Read file
-#
-# Return 0 if test succeds, else 1
-
-def test ():
-    writeFile("test.txt", "Hello ")
-    appendFile("test.txt", "World!")
-    readTest = readFile("test.txt")
-    if (readTest == "Hello World!"):
+def writeFile(fileName, toWrite):
+    """ Opens, writes and then closes a file. Creates a file if it doesn't exist and it will overwrite existing files
+    fileName (str): file to be accessed ie: "myfile.txt", "myFolder/myFile.txt".
+    toWrite (str): data that the file willl have after creation
+    return: 1 if error
+    return: 0 if success
+    """
+    try:
+        file_object = open(fileName, "w")
+        file_object.write(toWrite)
+        file_object.close()
         return 0
-    else:
+    except:
+        print("+++ Write operation failed +++")
         return 1
 
 
+def appendFile(fileName, toWrite):
+    """ Opens, appends and then closes a file. File must exist before appending
+    fileName (str): file to be accessed ie: "myfile.txt", "myFolder/myFile.txt".
+    toWrite (str): data to be appended to the file
+    return: 1 if error
+    return: 0 if success
+    """
+    try:
+        file_object = open(fileName, "a")
+        file_object.write(toWrite)
+        file_object.close()
+    except:
+        print("+++ Append operation failed +++")
+        return 1
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-       
+def fileManager(fileName, mode, data):
+    """ Manages the access and operaions in external files
+    Parameters:
+        fileName (str): file to be accessed
+        mode (str): "r" to read, "w" to write (erases previous data), "a" appends data to file
+        data (str): anything if read, data to append/wrie
+    Returns:
+        return: 1 if the operation failed
+        return: 0 if overwrites/appends to fileName file succeded
+        return: data read if mode == "r" 
+    """
+    # read and print file
+    if mode == "r" or mode == "R":
+        return readFile(fileName)
+    # appends to file
+    elif mode == "a" or mode == "A":
+        return appendFile(fileName, data)
+    # Create a new file
+    elif mode == "w" or mode == "W":
+        return writeFile(fileName, data)
+    else:
+        print("Mode not recognized")
+        return 1
